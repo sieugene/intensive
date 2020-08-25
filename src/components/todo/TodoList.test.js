a3a8da56a1f6abfa1e4b94cc1495478bece2ad83
sieugene <sieugene@mail.ru>
2020-08-25 21:01:00 +0300

    add test todoList component

--- src/components/todo/TodoList.test.js
+++ src/components/todo/TodoList.test.js
@@ -1,71 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Provider, useSelector } from "react-redux";
+import TodoList from "./TodoList";
+import { successTodos } from "../../ducks/todo";
+import store from "../../redux/root";
 
-export default function User(props) {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  async function fetchUserData(id) {
-    const response = await fetch("/" + id);
-    setUser(await response.json());
-    setLoading(true);
-  }
-
-  useEffect(() => {
-    fetchUserData(props.id);
-  }, [props.id]);
-
-  if (!loading) {
-    return "loading...";
-  }
-
-  return (
-    <details>
-      <summary>{user.name}</summary>
-      <strong>{user.age}</strong> years old
-      <br />
-      lives in {user.address}
-    </details>
+const renderTestedComponent = () => {
+  return render(
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
   );
-}
+};
 
-describe("User component", () => {
-  let container = null;
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useSelector: jest.fn(),
+}));
+
+describe("test TestedComponent components", () => {
   beforeEach(() => {
-    // подготавливаем DOM-элемент, куда будем рендерить
-    container = document.createElement("div");
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    // подчищаем после завершения
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-  });
-  it("renders user data", async () => {
-    const fakeUser = {
-      name: "Joni Baez",
-      age: "32",
-      address: "123, Charming Avenue",
-    };
-    jest.spyOn(global, "fetch").mockImplementation(() =>
-      Promise.resolve({
-        json: () => Promise.resolve(fakeUser),
-      })
-    );
-
-    // Используем act асинхронно, чтобы передать успешно завершённые промисы
-    await act(async () => {
-      render(<User id="123" />, container);
+    useSelector.mockImplementation((callback) => {
+      return callback(store.getState());
     });
-
-    expect(container.querySelector("summary").textContent).toBe(fakeUser.name);
-    expect(container.querySelector("strong").textContent).toBe(fakeUser.age);
-    expect(container.textContent).toContain(fakeUser.address);
-
-    // выключаем фиктивный fetch, чтобы убедиться, что тесты полностью изолированы
-    global.fetch.mockRestore();
+  });
+  it("should be show one todo", () => {
+    store.dispatch(successTodos([{ id: 1, title: "text" }]));
+    const { container } = renderTestedComponent();
+    const ul = container.querySelectorAll("ul").length;
+    expect(ul).toBe(1);
+  });
+  it("should be render", () => {
+    const { container } = renderTestedComponent();
+    expect(container).toBeInTheDocument();
   });
 });
